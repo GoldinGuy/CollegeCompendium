701218f937372f2ce615ae10318f44ffb0ccbfd5
Seth <sethgoldin27@gmail.com>
2021-07-10 00:24:32 -0400

    try embedding email in request

--- src/views/TempSplashPage.tsx
+++ src/views/TempSplashPage.tsx
@@ -46,17 +46,17 @@ const TempSplashPage = () => {
 														) {
 															console.log("Email is valid: ", email);
 															fetch(
-																`https://maker.ifttt.com/trigger/email_received/with/key/${process.env.REACT_APP_IFTTT_KEY}`,
+																`https://maker.ifttt.com/trigger/email_received/with/key/${process.env.REACT_APP_IFTTT_KEY}?&value1=${email}`,
 																{
 																	method: "POST",
-																     mode: "no-cors",
-																	headers: {
-																		"Content-Type": "application/json",
-																		// "Access-Control-Allow-Methods": "*"
-																	},
-																	body: JSON.stringify({
-																		value1: email
-																	})
+																	mode: "no-cors",
+																	// headers: {
+																	// 	"Content-Type": "application/json"
+																	// 	// "Access-Control-Allow-Methods": "*"
+																	// },
+																	// body: JSON.stringify({
+																	// 	value1: email
+																	// })
 																}
 															);
 															setEmail("");
