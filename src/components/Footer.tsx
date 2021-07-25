da23bf3c4a8e213cd2ad1155dc34c92f528bc86d
Seth <sethgoldin27@gmail.com>
2021-07-25 12:48:45 -0400

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,17 +8,17 @@ const Footer = () => {
 		<footer className="px-6 sm:px-12 md:px-24 pb-12 mx-auto max-w-screen bg-gray-50">
 			<div className="flex flex-col items-start justify-between pt-10 mt-10 border-t border-gray-100 md:flex-row md:items-center">
 				<p className="mb-2 font-semibold text-sm text-left text-gray-600 md:mb-0">
-					© 2021{" "}
 					<a href="https://collegecompendium.org" className="text-fuchsia-500">
 						College Compendium
+					</a>{" "}
+					is licensed under{" "}
+					<a href="https://github.com/GoldinGuy/CollegeCompendium/blob/master/LICENSE">
+						MIT
 					</a>
 				</p>
-				<span className="mb-0 text-xs text-left text-gray-600 md:mb-0 flex flex-col md:flex-row justify-between">
+				<span className="mb-0 text-xs lg:text-sm text-left text-gray-600 md:mb-0 flex flex-col md:flex-row justify-between">
 					<p className="mb-2 md:mb-0 pr-6">
-						<a href="https://github.com/GoldinGuy/CollegeCompendium/blob/master/LICENSE">
-							MIT licensed
-						</a>
-						. College Compendium is not affiliated with any colleges or classes.
+						College Compendium is not affiliated with any colleges or classes.
 						Please do <i>not</i> attempt to contact professors.
 					</p>
 					<Link
@@ -26,8 +26,7 @@ const Footer = () => {
 						className="mb-2 md:mb-0 hover:underline pr-6 flex items-center"
 						// href="https://www.youtube.com/watch?v=oHg5SJYRHA0"
 					>
-						Terms of Service{" "}
-						<FontAwesomeIcon icon={faInfoCircle} className="ml-2 inline" />
+						Terms	<FontAwesomeIcon icon={faInfoCircle} className="ml-2 inline" />
 					</Link>
 					<a
 						className="mb-2 md:mb-0 hover:underline pr-6 flex items-center"
