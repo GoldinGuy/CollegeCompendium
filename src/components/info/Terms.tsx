fd8296addc90a8ce717e8dfe49d78b53c70f7098
GoldinGuy <sethgoldin27@gmail.com>
2022-02-17 14:29:53 -0500

    update cr

--- src/components/info/Terms.tsx
+++ src/components/info/Terms.tsx
@@ -1,5 +1,5 @@
 /*  College Compendium Component File
-Copyright (C) 2021 Seth Goldin & Samuel Crombie
+Copyright (C) 2022 Seth Goldin & Samuel Crombie
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
 by the Free Software Foundation, either version 3 of the License, or
